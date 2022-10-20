e499bddb02c8f4a046cd54ad05d11147f26f7baf
Patricker75 <biet.vui@gmail.com>
2022-10-20 13:13:43 -0500

    Add project files.

---
+++ Library/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.Models
+{
+    public partial class Member
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+        public char MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public int PhoneNum { get; set; }
+    }
+}
---
+++ Library/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Models
+{
+    public partial class Room
+    {
+        public int ID { get; set; }
+
+        public string Location { get; set; }
+
+        [Column("availability")]
+        public bool IsAvailable { get; set; }
+        
+        [Column("room_type")]
+        public short RoomType { get; set; }
+    }
+}
