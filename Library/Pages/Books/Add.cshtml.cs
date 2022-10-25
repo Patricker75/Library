4bf2717231026f3e783083f4b5250e23833cca95
Patricker75 <biet.vui@gmail.com>
2022-10-24 19:50:21 -0500

    attempting to get book input form to work

--- Library/Pages/Books/Add.cshtml.cs
+++ Library/Pages/Books/Add.cshtml.cs
@@ -1,3 +1,4 @@
+using Library.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,53 @@ namespace Library.Pages.Books
 {
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public string Title { get; set; } = string.Empty;
+
+        [BindProperty]
+        public int RowCount { get; set; } = 0;
+
+        [BindProperty]
+        public IList<Author> Authors { get; set; } = default!;
+
+        [BindProperty]
+        public string TestStr { get; set; } = string.Empty;
+
         public void OnGet()
         {
+        }
+
+        // Returns True if all Authors Added have a first and last name
+        private bool ValidateAuthors()
+        {
+            foreach(var author in Authors)
+            {
+                if (string.IsNullOrEmpty(author.FirstName) || string.IsNullOrEmpty(author.LastName))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public IActionResult OnPost()
+        {
+            bool validForm = true;
+            if (!ValidateAuthors())
+            {
+                validForm = false;
+            }
+
+
+            if (validForm)
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
