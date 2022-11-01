26accaf726de655c0b20d713c773e4882d81b0e4
Github2287 <xvictorx588@gmail.com>
2022-11-01 14:19:58 -0500

    Room Page

--- Library/Pages/Rooms/Create.cshtml.cs
+++ Library/Pages/Rooms/Create.cshtml.cs
@@ -7,17 +7,31 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Library.Data;
 using Library.Models;
+using System.Xml.Linq;
 
 namespace Library.Pages.Rooms
 {
     public class CreateModel : PageModel
     {
-        private readonly Library.Data.LibraryContext _context;
+       
 
+        [BindProperty]
+        public int RoomType { get; set; } = -1;
+
+        [BindProperty]
+        public string Location { get; set; } = string.Empty;
+
+        [BindProperty]
+        public bool IsAvailable { get; set; } = true;
+
+        [BindProperty]
+        public string ErrorMessage { get; set; } = string.Empty;
         public CreateModel(Library.Data.LibraryContext context)
         {
             _context = context;
         }
+
+        private readonly Library.Data.LibraryContext _context;
 
         public IActionResult OnGet()
         {
@@ -29,17 +43,46 @@ namespace Library.Pages.Rooms
         
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        
+        private bool VerifyForm()
         {
-          if (!ModelState.IsValid)
+            // Check if name is filled
+            if (RoomType<0)
             {
-                return Page();
+                return false;
             }
 
-            _context.Room.Add(Room);
-            await _context.SaveChangesAsync();
+            // Check if location is filled
+            if (string.IsNullOrEmpty(Location))
+            {
+                return false;
+            }
 
-            return RedirectToPage("./Index");
+            return true;
+        }
+        public IActionResult OnPost()
+        {
+            if (VerifyForm())
+            {
+                Room newRoom = new Room()
+                {
+                    RoomType = (RoomType)RoomType,
+                    Location = Location,
+                    IsAvailable = IsAvailable
+                };
+
+
+
+                return RedirectToPage("Create");
+            }
+            else
+            {
+                ErrorMessage = "A Required Field has Been Left Blank";
+
+                ModelState.Clear();
+
+                return Page();
+            }
         }
     }
 }
