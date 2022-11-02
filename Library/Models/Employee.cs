a30705007b33f841d6fbfe984f473244785314fe
Patricker75 <biet.vui@gmail.com>
2022-11-02 15:33:01 -0500

    working on Employee still adding backend and DB insert

--- Library/Models/Employee.cs
+++ Library/Models/Employee.cs
@@ -8,9 +8,13 @@ namespace Library.Models
 	{
 		public int ID { get; set; }
 
+		[Column("username")]
 		public string Username { get; set; }
 
-		[Column("first_name")]
+        [Column("password")]
+        public string Password { get; set; }
+
+        [Column("first_name")]
 		public string FirstName { get; set; }
 
 		[Column("middle_initial")]
@@ -45,6 +49,7 @@ namespace Library.Models
 		[ForeignKey("Employee")]
 		[Column("supervisor_id")]
 		public int SupervisorID { get; set; }
+
 
         public bool IsValid()
         {
--- Library/Pages/Employees/Add.cshtml.cs
+++ Library/Pages/Employees/Add.cshtml.cs
@@ -1,6 +1,8 @@
 using Library.Data;
+using Library.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Library.Pages.Employees
 {
@@ -8,6 +10,9 @@ namespace Library.Pages.Employees
     {
         [BindProperty]
         public string Username { get; set; } = string.Empty;
+
+        [BindProperty, MaxLength(10)]
+        public string Password { get; set; } = string.Empty;
 
         [BindProperty]
         public string FirstName { get; set; } = string.Empty;
@@ -34,10 +39,13 @@ namespace Library.Pages.Employees
         public DateTime HireDate { get; set; } = DateTime.Now;
 
         [BindProperty]
-        public string JobTitle { get; set; }
+        public string JobTitle { get; set; } = string.Empty;
 
         [BindProperty]
-        public decimal Salary { get; set; }
+        public decimal Salary { get; set; } = decimal.Zero;
+
+        [BindProperty]
+        public string ErrorMessage { get; set; } = string.Empty;
  
         private readonly LibraryContext _context;
 
@@ -49,6 +57,79 @@ namespace Library.Pages.Employees
         public void OnGet()
         {
             BirthDate = new DateTime(HireDate.Year - 18, HireDate.Month, HireDate.Day);
+        }
+
+        private bool VerifyForm()
+        {
+            // Checks if username, first name, last name, job title, password, address, phone number is filled
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(FirstName) || string.IsNullOrEmpty(LastName)
+                || string.IsNullOrEmpty(JobTitle) || string.IsNullOrEmpty(Password) || string.IsNullOrEmpty(Address)
+                || string.IsNullOrEmpty(PhoneNumber))
+            {
+                return false;
+            }
+
+            // Check that Hire Date occurs after Birth Date
+            if (BirthDate >= HireDate)
+            {
+                return false;
+            }
+
+            // Checks if Hire Date occurs 18 years after Birth Date
+            TimeSpan diffenence = HireDate - BirthDate;
+            if (diffenence.TotalDays < 18 * 365) // Comparing days between hiring and birthday to number of days in 18 years
+            {
+                return false;
+            }
+
+            // Checks that Salary is valid
+            if (Salary <= 0)
+            {
+                return false;
+            }
+
+            // Checks that Gender is chosen
+            if (Gender < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public IActionResult OnPost()
+        {
+            if(VerifyForm())
+            {
+                Employee newEmployee = new Employee()
+                {
+                    Username = Username,
+                    Password = Password,
+
+                    FirstName = FirstName,
+                    LastName = LastName,
+                    
+                    Address = Address,
+                    PhoneNum = PhoneNumber,
+
+                    JobTitle = JobTitle,
+                    Salary = Salary,
+                    
+                    Gender = (Gender)Gender,
+                    BirthDate = BirthDate,
+                    HireDate = HireDate
+                };
+
+                return RedirectToPage("Add");
+            }
+            else
+            {
+                ErrorMessage = "A Required Field has Been Left Blank";
+
+                ModelState.Clear();
+
+                return Page();
+            }
         }
     }
 }
