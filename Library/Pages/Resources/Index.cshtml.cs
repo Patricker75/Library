8100f3ffb438293cb3594c586bcb72fd370f82a5
SeanPletan <AvrPtl1453@gmail.com>
2022-11-18 23:22:55 -0600

    Created the page for Resource data entry and added test data within azure data studio

---
+++ Library/Pages/Resources/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using Library.Data;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Library.Pages.Resources
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public IList<Resource> Resources { get; set; }
+
+        private readonly LibraryContext _context;
+
+        public IndexModel(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (_context.Resources != null)
+            {
+                Resources = _context.Resources.ToList();
+            }
+
+            return Page();
+        }
+    }
+}
