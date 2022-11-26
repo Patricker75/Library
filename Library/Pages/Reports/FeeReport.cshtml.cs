fa80b060cba16470d2f692f285cd7ed8cd679a2b
SeanPletan <AvrPtl1453@gmail.com>
2022-11-26 15:27:27 -0600

    Began the process for adding fee report. displayed link and added href linking to a new tab for the null member role in resources.index.cshtml

---
+++ Library/Pages/Reports/FeeReport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Library.Pages.Reports
+{
+    public class FeeReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
